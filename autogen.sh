59d1a23837cf26f45300f681fd02c298896c828d
grendello <grendello@868fd3cd-e145-0410-a037-ab4dd4f9362a>
2008-02-11 14:59:25 +0000

    Use autoreconf in autogen.sh; fix the initialization in configure.ac

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,9 @@
-glib-gettextize --force --copy ||
-  { echo "**Error**: glib-gettextize failed."; exit 1; }
+#!/bin/sh
+#glib-gettextize --force --copy ||
+#  { echo "**Error**: glib-gettextize failed."; exit 1; }
 
-aclocal
-automake -a
-autoconf
+#aclocal
+#automake -a
+#autoconf
+autoreconf -f -i
 ./configure --enable-maintainer-mode $*
