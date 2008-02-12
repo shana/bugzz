80f324672b995b036e2dd6d09f7c9526ac0df00f
grendello <grendello@868fd3cd-e145-0410-a037-ab4dd4f9362a>
2008-02-12 18:50:32 +0000

    Added one more event to WebIO. Added loading buglist code (rudimentary).

---
+++ library/Bugzz.Network/DownloadStartedEventHandler.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace Bugzz.Network
+{
+	public delegate void DownloadStartedEventHandler (object sender, DownloadStartedEventArgs a);
+}
--- library/webtest.cs
+++ library/webtest.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Threading;
 
 using Bugzz.Bugzilla;
@@ -11,6 +13,8 @@ class App
 		Bugzilla bugz = new Bugzilla (args [0]);
 		bugz.WebIO.DownloadProgress += new DownloadProgressEventHandler (OnDownloadProgress);
 		bugz.WebIO.DownloadEnded += new DownloadEndedEventHandler (OnDownloadEnded);
+		bugz.WebIO.DownloadStarted += new DownloadStartedEventHandler (OnDownloadStarted);
+		bugz.WebIO.DocumentRetrieveFailure += new DocumentRetrieveFailureEventHandler (OnDocumentRetrieveFailure);
 		
 		Thread t = new Thread (App.StartRequest);
 		Console.WriteLine ("Starting thread.");
@@ -20,15 +24,36 @@ class App
 		Console.WriteLine ("Refresh finished.");
 	}
 
+	static void ShowProgress (long start, long end)
+	{
+		Console.Write ("\rCompleted: {0:F2}%", ((double)start / (double)end) * 100);
+	}
+	
 	static void OnDownloadProgress (object sender, DownloadProgressEventArgs args)
 	{
-		Console.Write ("\rCompleted: {0:F2}%", ((double)args.CurrentCount / (double)args.MaxCount) * 100);
+		ShowProgress (args.CurrentCount, args.MaxCount);
 	}
 
+	static void OnDownloadStarted (object sender, DownloadStartedEventArgs args)
+	{
+		ShowProgress (0, args.Response.ContentLength);
+	}
+	
 	static void OnDownloadEnded (object sender, DownloadEndedEventArgs args)
 	{
+		long end = args.Response.ContentLength;
+		ShowProgress (end, end);
+		
 		Console.WriteLine ();
 		Console.WriteLine ("Download ended. Status: {0}", args.Response.StatusCode);
+	}
+
+	static void OnDocumentRetrieveFailure (object sender, DocumentRetrieveFailureEventArgs args)
+	{
+		Console.WriteLine ();
+		HttpWebResponse response = args.Request.GetResponse () as HttpWebResponse;
+		
+		Console.WriteLine ("Failed to download document. Status: {0}", response != null ? response.StatusCode.ToString () : "unknown");
 	}
 	
 	static void StartRequest (object data)
@@ -38,8 +63,14 @@ class App
 
 		if (bugz == null)
 			return;
-		
-		bugz.Refresh ();
+
+		Dictionary <string, string> vars = new Dictionary <string, string> {
+			{"classification", "Mono"},
+			{"product", "Mono: Class Libraries"},
+			{"component", "Sys.Web"}
+		};
+
+		bugz.GetBugList (vars);
 		Console.WriteLine ("Request ended.");
 	}
 	
