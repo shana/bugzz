640b594cc891f045d935778a6560f078de9435c6
grendello <grendello@868fd3cd-e145-0410-a037-ab4dd4f9362a>
2008-02-12 02:45:39 +0000

    Do not treat <option> tags as empty ones

--- 3rdparty/HtmlAgilityPack/HtmlNode.cs
+++ 3rdparty/HtmlAgilityPack/HtmlNode.cs
@@ -89,7 +89,7 @@ namespace HtmlAgilityPack
 			ElementsFlags.Add("form", HtmlElementFlag.CanOverlap | HtmlElementFlag.Empty);
 
 			// they sometimes contain, and sometimes they don 't...
-			ElementsFlags.Add("option", HtmlElementFlag.Empty);
+			//ElementsFlags.Add("option", HtmlElementFlag.Empty);
 
 			// tag whose closing tag is equivalent to open tag:
 			// <p>bla</p>bla will be transformed into <p>bla</p>bla
