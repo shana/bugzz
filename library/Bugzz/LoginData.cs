672e03063e83e9586791ae3a01084399356fb464
grendello <grendello@868fd3cd-e145-0410-a037-ab4dd4f9362a>
2008-02-13 16:54:29 +0000

    Login stuff begins

---
+++ library/Bugzz/LoginData.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Bugzz
+{
+	public class LoginData
+	{
+		public string Url /*{ get; set; }*/;
+		public string UsernameField /*{ get; set; }*/;
+		public string PasswordField /*{ get; set; }*/;
+		public string Username /*{ get; set; }*/;
+		public string Password /*{ get; set; }*/;
+
+		public LoginData ()
+		{
+		}
+	}
+}
