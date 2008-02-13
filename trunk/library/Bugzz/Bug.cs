89061f7301779ecbb2c75eb387ce64b2cc8f20bf
grendello <grendello@868fd3cd-e145-0410-a037-ab4dd4f9362a>
2008-02-13 00:13:10 +0000

    Let's stick to this version :)

--- trunk/library/Bugzz/Bug.cs
+++ trunk/library/Bugzz/Bug.cs
@@ -1,38 +1,46 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Xml;
 
 namespace Bugzz
 {
 	public class Bug
 	{
-		public int ID;
-		public string AssignedTo;
-		public string Status;
-		public string Description;
-
-		internal Bug (XmlNode bugNode, XmlNamespaceManager ns)
-		{
-			XmlNode node = bugNode.SelectSingleNode ("bz:id", ns);
-			ID = int.Parse (node.InnerText);
-			node = bugNode.SelectSingleNode ("bz:assigned_to", ns);
-			if (node != null)
-				AssignedTo = node.InnerText;
-			node = bugNode.SelectSingleNode ("bz:bug_status", ns);
-			if (node != null)
-				Status = node.InnerText;
-			node = bugNode.SelectSingleNode ("bz:short_desc", ns);
-			if (node != null)
-				Description = node.InnerText;
+		public int ID {
+			get;
+			set;
 		}
 
-		public Bug (int id, string assignedto, string status, string description)
+		public string Severity {
+			get;
+			set;
+		}
+
+		public string Priority {
+			get;
+			set;
+		}
+
+		public string OpSys {
+			get;
+			set;
+		}
+
+		public string Resolution {
+			get;
+			set;
+		}
+
+		public string ShortDesc {
+			get;
+			set;
+		}
+
+		public string URL {
+			get;
+			set;
+		} 
+
+		public Bug ()
 		{
-			ID = id;
-			AssignedTo = assignedto;
-			Status = status;
-			Description = description;
 		}
 	}
 }
